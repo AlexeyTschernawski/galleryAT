11731b1738e332eaf2e01fad59828467453d3087
AlexeyTschernawski <allexxem@gmail.com>
2024-06-15 10:48:47 +0200

    update 15.06

---
+++ imageDataArchive.js
@@ -0,0 +1,98 @@
+const imageData = [
+    {
+        src: './img/archive/1.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/2.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/3.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+    {
+        src: './img/archive/4.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/5.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/6.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/7.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/8.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/9.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/10.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/11.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/12.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/13.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+
+    {
+        src: './img/archive/14.jpg',
+        name: 'Morning',
+        description: 'Description about picture',
+        dimensions: '70 x 120 cm'
+    },
+];
